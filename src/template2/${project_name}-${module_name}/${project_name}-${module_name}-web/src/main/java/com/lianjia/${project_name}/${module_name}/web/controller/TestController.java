a00716465842446f141e903b6720afa094ba19ef
lichao <408964891@qq.com>
2019-01-07 17:21:52 +0800

    提交代码

---
+++ src/template2/${project_name}-${module_name}/${project_name}-${module_name}-web/src/main/java/com/lianjia/${project_name}/${module_name}/web/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.lianjia.${project_name}.${module_name}.web.controller;
+
+import com.lianjia.ca.driver.common.JsonResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@Controller
+@RequestMapping("/api/test")
+public class TestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ResponseBody
+    public JsonResult index() {
+        return JsonResult.successResult("hello");
+    }
+
+
+}
